ca35e260d02734dfbbfb21e9d7ac91f5ee7b6d79
Nicolas <nforneirorosas@gmail.com>
2025-10-20 18:52:23 -0400

    Continue working

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,25 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4))
+  return res.status(200).json({message: "Here all the books."});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+    res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  for (let id in books) {
+    if (books[id].author === author) {
+        res.send(books[id]);
+    }
+  }
 });
 
 // Get all books based on title
